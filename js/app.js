ce6c17c29ca219e2f66648c5cc385ee5f0a5fcec
Behtsa <behtsabet14@gmail.com>
2018-03-08 11:21:51 -0600

    localStorage

--- js/app.js
+++ js/app.js
@@ -1,6 +1,54 @@
 let counterItems = document.getElementById("counterItems");
 let counter = parseInt(counterItems.innerText);
 
+
+const addToCart = (product) => {
+  console.log(product)
+  let filterProduct = data.products.filter(function(obj) {
+    if(obj.id === product) {
+      return obj;
+    }
+  });
+
+  let productDetails = filterProduct[0];
+  localStorage.setItem('productDetails', JSON.stringify(productDetails));
+  /* 
+  2) Guardar mi producto en algun lugar
+  */
+}
+
+const removeFromCart = () => {
+  console.log("quito")
+  /* cuando agrego a carrito, tengo que:
+  2) Borrar mi producto de algun lugar
+  */
+}
+
+const increaseCounter = (product) => {
+  counter += 1;
+  counterItems.innerText = counter;
+  addToCart(product);
+}
+
+const decreaseCounter = () => {
+  counter -= 1;
+  counterItems.innerText = counter;
+  removeFromCart();
+}
+
+const changeButtonStatus = (product, event) => {
+  let element = event.target
+  let buttonText = element.firstChild.data;
+
+  if(buttonText === "Agregar a carrito") {
+    element.innerText = "Remover del carrito";
+    increaseCounter(product);
+  } else {
+    element.innerText = "Agregar a carrito";
+    decreaseCounter();
+  }
+}
+
 const drawProducts = data => {
   let products = data.products;
   let productsContainer = document.getElementById("products-container");
@@ -27,42 +75,3 @@ const createProductHTML = product => {
 }
 
 drawProducts(data);
-
-const addToCart = () => {
-  console.log("agrego");
-  /* 
-  2) Guardar mi producto en algun lugar
-  */
-}
-
-const removeFromCart = () => {
-  console.log("quito")
-  /* cuando agrego a carrito, tengo que:
-  2) Borrar mi producto de algun lugar
-  */
-}
-
-const increaseCounter = () => {
-  counter += 1;
-  counterItems.innerText = counter;
-  addToCart();
-}
-
-const decreaseCounter = () => {
-  counter -= 1;
-  counterItems.innerText = counter;
-  removeFromCart();
-}
-
-const changeButtonStatus = (product, event) => {
-  let element = event.target
-  let buttonText = element.firstChild.data;
-
-  if(buttonText === "Agregar a carrito") {
-    element.innerText = "Remover del carrito";
-    increaseCounter();
-  } else {
-    element.innerText = "Agregar a carrito";
-    decreaseCounter();
-  }
-}
