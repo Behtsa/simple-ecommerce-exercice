ca4783cff54f7447bb652bb8909979b0d499f334
Behtsa <behtsabet14@gmail.com>
2018-03-08 12:12:47 -0600

    get Total Amount to Pay

--- js/checkout.js
+++ js/checkout.js
@@ -1,10 +1,26 @@
-let container = document.getElementById()
+let totalTR = document.getElementById('total');
+let tabody = document.getElementById('body-table');
+let product = localStorage.getItem('productDetails');
+productDetails= JSON.parse(product);
+console.log(productDetails);
 
-function calculateTotal() {
-	let product = localStorage.getItem('productDetails');
-	productDetails= JSON.parse(product);
-	console.log(productDetails);
+const calculateTotal = productDetails => {
+  let tableTemplate = ` `;
+  let total = 0;
+  let totalGap = ` `;
+  productDetails.forEach(product => {
+    total += product.price;
+    tableTemplate += `<tr>
+    <th scope="row">${product.title}</th>
+    <td>${product.price}</td>
+    </tr>`;
+    tabody.innerHTML = tableTemplate;
+  })
+  totalGap += `
+    <td><strong>TOTAL: </strong>${total}</td>`;
+    totalTR.innerHTML = totalGap;
 }
 
-calculateTotal();
+
+calculateTotal(productDetails);
 
